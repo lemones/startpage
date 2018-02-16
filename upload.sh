e7166cdaac7178433f7c5c2be8627fd6f09582ff
lemones <citronster@gmail.com>
2018-02-16 13:33:28 +0100

    16 Feb 2018

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp index.html src.js style.css wall.png lemones@badanka.org:public_html/ > /dev/null
+scp date.format.js index.html src.js style.css wall.png lemones@badanka.org:public_html/ > /dev/null
 echo -e "\033[32m::::> \033[34mUploaded to: http://badanka.org/\033[39m"
